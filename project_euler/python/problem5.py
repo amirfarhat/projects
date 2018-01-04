3a09c700499bbd15f0b701c3e67f22ebe29172fe
amirfarhat <amfar19@gmail.com>
2018-01-04 14:16:56 -0500

    Solved python problem 5

--- project_euler/python/problem5.py
+++ project_euler/python/problem5.py
@@ -9,36 +9,58 @@ What is the smallest positive number that is evenly
 divisible by all of the numbers from 1 to 20?
 
 -------------------------------------------------------
-STATUS: ONGOING
-
+STATUS: COMPLETE
+ANSWER: 232792560
 -------------------------------------------------------
-1 = 1
-2 = 2
-3 = 3
-4 = 2 ** 2
-5 = 5
-6 = 2 * 3
-7 = 7
-8 = 2 ** 3
-9 = 3 ** 2
-10 = 2 * 5
-
-1*2*3*4*5*6*7*8*9*10 = 
-1 * (2**(1+2+1+3+1)) * (3**(1+1+2)) * (5**(1+1)) * 7
-
-has 
 """
 
-def divides_one_through_twenty(num):
-    for div in range(1, 21):
-        if num % div != 0:
-            return False
-    return True
+def prime_factors(num):
+	"""
+	Returns a dictionary of the prime factors
+	of the number num where keys are prime factors
+	and values are their respective multiplicities
+	"""
+	facs = dict()
+	div = 2
+	while num != 1:
+		while num % div == 0: # still divisible
+			num /= div # decompose
+			if div not in facs:
+				facs[div] = 1
+			else:
+				facs[div] += 1
+		div += 1
+	return facs
 
-num = 2
-while not divides_one_through_twenty(num):
-    # print(num)
-    num += 2
+def compactify_prime_factors(divisor_list):
+	"""
+	Returns a factors dictionary new_facs which has
+	the prime factors of divisor_list, but has the 
+	maximum multiplicity of each prime factor
+	"""
+	new_facs = dict()
+	for divisor in divisor_list:
+		facs = prime_factors(divisor)
+		for factor in facs:
+			if factor not in new_facs:
+				new_facs[factor] = 1
+			else:
+				new_facs[factor] = max(new_facs[factor], facs[factor])
+	return new_facs
 
-print(num)
+def construct_num_from_dict(factors):
+	"""
+	Takes in a factors dictionary with form same as
+	output from prime_factors, and returns the number
+	made from those factors and their multiplicities
+	"""
+	result = 1
+	for factor in factors:
+		multiplicity = factors[factor]
+		result *= factor ** multiplicity
+	return result
 
+if __name__ == '__main__':
+	divisor_list = [x for x in range(1, 21)]
+	compact_primes = compactify_prime_factors(divisor_list)
+	print(construct_num_from_dict(compact_primes))
